8160eae4bb41dced713175726713de399c58d14e
Dylan897-alt <dylan.arthur0305@gmail.com>
2025-06-16 11:03:58 +0700

    bagi tugas

--- core/src/main/java/io/github/some_example_name/object/GameObject.java
+++ core/src/main/java/io/github/some_example_name/object/GameObject.java
@@ -1,11 +1,22 @@
 package io.github.some_example_name.object;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import io.github.some_example_name.Collidable;
 
-public abstract class GameObject {
+public abstract class GameObject implements Collidable {
     //nanti bullet, crosshair pindah sini (extend GameObject)
 //    public void draw(SpriteBatch batch) {
 //        sprite.draw(batch);
+//    }
+//    @Override
+//    public Rectangle getHitbox(){
+//        return new Rectangle(
+//            sprite.getX(),
+//            sprite.getY(),
+//            sprite.getWidth(),
+//            sprite.getHeight()
+//        );
 //    }
     public abstract void update(float delta);
 }
