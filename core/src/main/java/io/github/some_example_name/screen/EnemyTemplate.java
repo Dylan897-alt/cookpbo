1eed13d30eb419f8e33d45b52b83c8cf3a8178b3
Dylan897-alt <dylan.arthur0305@gmail.com>
2025-06-23 23:50:39 +0700

    fix weapon copying issue

--- core/src/main/java/io/github/some_example_name/screen/EnemyTemplate.java
+++ core/src/main/java/io/github/some_example_name/screen/EnemyTemplate.java
@@ -4,22 +4,24 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
 import io.github.some_example_name.character.Enemy;
-import io.github.some_example_name.weapon.Weapon;
+import io.github.some_example_name.weapon.*;
+
+import java.security.cert.CertificateParsingException;
 
 public class EnemyTemplate {
     private float hp;
     private float exp;
-    private Weapon weapon;
+    private int type;
     private Texture bulletTexture;
     private Texture spriteSheet;
     private int frameWidth;
     private int frameHeight;
     private float frameDuration;
 
-    public EnemyTemplate(int frameWidth, int frameHeight, float frameDuration, float hp, float exp, Weapon weapon, Texture spriteSheet, Texture bulletTexture){
+    public EnemyTemplate(int frameWidth, int frameHeight, float frameDuration, float hp, float exp, int type, Texture spriteSheet, Texture bulletTexture){
         this.hp = hp;
         this.exp = exp;
-        this.weapon = weapon;
+        this.type = type;
         this.spriteSheet = spriteSheet;
         this.bulletTexture = bulletTexture;
         this.frameWidth = frameWidth;
@@ -28,6 +30,18 @@ public class EnemyTemplate {
     }
 
     public Enemy createEnemy(Vector2 spawnPos){
+        Weapon weapon = null;
+        if(type == 1){
+            weapon = new SingleShotWeapon(1.2f);
+        } else if(type == 2){
+            weapon = new TripleShotWeapon(2.0f);
+        } else if(type == 3){
+            weapon = new SpamShotWeapon(4.0f);
+        } else if(type == 4){
+            weapon =  new FiveShotWeapon(3f);
+        } else if(type == 5){
+            weapon = new BomberWeapon(2.0f);
+        }
         return new Enemy(frameWidth, frameHeight, frameDuration, hp, exp, weapon, spriteSheet, bulletTexture, spawnPos);
     }
 }
