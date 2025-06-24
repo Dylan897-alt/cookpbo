f7018bb2d90063f8e197a4a257ea9c47872b8e2b
VennZx7 <boychildren7.ew@gmail.com>
2025-06-24 14:08:24 +0700

    bullet, mainmenu, stage1, dan enemymanager

--- core/src/main/java/io/github/some_example_name/ShooterGame.java
+++ core/src/main/java/io/github/some_example_name/ShooterGame.java
@@ -1,14 +1,15 @@
 package io.github.some_example_name;
 
 import com.badlogic.gdx.Game;
+import io.github.some_example_name.screen.MainMenu;
 import io.github.some_example_name.screen.Stage1;
 
 public class ShooterGame extends Game {
-    public final float VIRTUAL_WIDTH = 8;
-    public final float VIRTUAL_HEIGHT = 5;
+    public static final float VIRTUAL_WIDTH = 8;
+    public static final float VIRTUAL_HEIGHT = 5;
 
     @Override
     public void create() {
-        this.setScreen(new Stage1(this));
+        this.setScreen(new MainMenu(this));
     }
 }
--- core/src/main/java/io/github/some_example_name/screen/EnemyManager.java
+++ core/src/main/java/io/github/some_example_name/screen/EnemyManager.java
@@ -3,11 +3,10 @@ package io.github.some_example_name.screen;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
+import io.github.some_example_name.ShooterGame;
 import io.github.some_example_name.character.Enemy;
 import io.github.some_example_name.character.Player;
-import io.github.some_example_name.object.Bullet;
 import io.github.some_example_name.object.BulletManager;
-import io.github.some_example_name.weapon.*;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -18,26 +17,54 @@ public class EnemyManager {
     private ArrayList<Enemy> enemies = new ArrayList<>();
     private Random random = new Random(); //pake cara lain gpp
     private float cooldown = 1f;
+    private int totalSpawneds1 = 0;//batas spawn
+    private final int MAX_SPAWNs1 = 20;
+
 
     public EnemyManager(){
-//        templateStage1.add(new EnemyTemplate(713, 541, .2f, 10, 2, 1, new Texture("stage1_single_shot.png"), new Texture("2.png")));
-//        templateStage1.add(new EnemyTemplate(734, 672, .2f, 10, 2, 2, new Texture("stage1_melee.png"), new Texture("2.png")));
-//        templateStage1.add(new EnemyTemplate(48, 32, .1f, 10, 2, 3, new Texture("Canine_Black_Attack.png"), new Texture("2.png")));
-//        templateStage1.add(new EnemyTemplate(48, 32, .1f, 10, 2,4, new Texture("Canine_Black_Attack.png"), new Texture("2.png")));
         templateStage1.add(new EnemyTemplate(48, 32, .1f, 10, 2, 5, new Texture("Canine_Black_Attack.png"), new Texture("2.png")));
     }
 
     public void handleSpawnStage1(float delta){
+        if (totalSpawneds1 >= MAX_SPAWNs1) return; //
+
         cooldown -= delta;
-        //koordinatnya benerin ward
+
         if (cooldown <= 0) {
             cooldown = 1f + random.nextFloat() * 2f;
-            float xPosition = random.nextFloat() * 8;
-            float yPosition = random.nextFloat() * 5;;
-            Vector2 spawnPos = new Vector2(xPosition, yPosition);
+
+            float enemyWidth = 0.8f;
+            float enemyHeight = 0.6f;
+            final float margin = 0.75f;
+
+            int side = random.nextInt(4);
+            float x = 0, y = 0;
+
+            switch(side) {
+                case 0: // kiri
+                    x = 0;
+                    y = margin + random.nextFloat() * (ShooterGame.VIRTUAL_HEIGHT - 2 * margin - enemyHeight);
+                    break;
+                case 1: // kanan
+                    x = ShooterGame.VIRTUAL_WIDTH - enemyWidth;
+                    y = margin + random.nextFloat() * (ShooterGame.VIRTUAL_HEIGHT - 2 * margin - enemyHeight);
+                    break;
+                case 2: // atas
+                    x = margin + random.nextFloat() * (ShooterGame.VIRTUAL_WIDTH - 2 * margin);
+                    y = ShooterGame.VIRTUAL_HEIGHT - enemyHeight - margin;
+                    break;
+                case 3: // bawah
+                    x = margin + random.nextFloat() * (ShooterGame.VIRTUAL_WIDTH - 2 * margin);
+                    y = 0;
+                    break;
+            }
+
+            Vector2 spawnPos = new Vector2(x, y);
             spawnEnemy(spawnPos);
+            totalSpawneds1++;
         }
     }
+
 
     public void handleSpawnStage2(float delta){
 
